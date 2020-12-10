246be49b85060c3b40a2f9d877636bf4bc5e4f0f
M15 <ismaelmelgar1999@gmail.com>
2020-12-10 13:38:56 +0100

    Main parte B

--- src/main/java/pablomingolla/racionalismaelpablo/AppRacional.java
+++ src/main/java/pablomingolla/racionalismaelpablo/AppRacional.java
@@ -16,24 +16,26 @@ public class AppRacional {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Racional z=new Racional(4,5);
-        Racional k=new Racional(3,4);
-        
-        
-        if(Racional.igualdad(k, k)){
-            System.out.println("Los racionales "+z.toString()+" y "+k.toString()+" son iguales.");
-        }else{
-            System.out.println("Los racionales "+z.toString()+" y "+k.toString()+" no son iguales.");
+        Racional z = new Racional(4, 5);
+        Racional k = new Racional(3, 4);
+
+        Racional aleatorio = new Racional();
+        aleatorio = aleatorio.aleatorio();
+        System.out.printf("El numero racional aleatorio es %.3f\n", (double) aleatorio.nominador / aleatorio.denominador);
+        System.out.printf("El numero racional z es %.3f\n", (double) z.nominador / z.denominador);
+        System.out.printf("El numero racional x es %.3f\n", (double) k.nominador / k.denominador);
+
+        System.out.println("La suma de los numeros z y k son " + z.suma(k));
+
+        if (Racional.igualdad(z, k)) {
+            System.out.println("Los racionales " + z.toString() + " y " + k.toString() + " son iguales.");
+        } else {
+            System.out.println("Los racionales " + z.toString() + " y " + k.toString() + " no son iguales.");
         }
-        
-        
-        Racional a=new Racional();
-        a= a.division(z, k);
-        a.toString();
+
+        Racional a = new Racional();
+        a = a.division(z, k);
+        a.imprimirConsola();
     }
-    
-    
-    
-    
-    
+
 }
